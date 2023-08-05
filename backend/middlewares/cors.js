5771b824b6891cacc0223f9587b596376d24a9f4
Anton Pasyuk <atadrakula25@yandex.ru>
2023-08-05 15:43:09 +0300

    Develop/p15 cors f+

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -3,6 +3,15 @@ const allowedCors = [
   'localhost:3000',
 ];
 
+// const allowedHeaders = [
+//   'Content-Type',
+//   'Origin',
+//   'Accept',
+//   'Authorization',
+//   'Access-Control-Request-Method',
+//   'Access-Control-Request-Headers'
+// ];
+
 module.exports = (req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
@@ -11,6 +20,7 @@ module.exports = (req, res, next) => {
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
 
   if (method === 'OPTIONS') {
@@ -19,7 +29,7 @@ module.exports = (req, res, next) => {
     // разрешаем кросс-доменные запросы с этими заголовками
     res.header('Access-Control-Allow-Headers', requestHeaders);
 
-    return res.end();
+    return res.status(200).end();
   }
   return next();
 };
