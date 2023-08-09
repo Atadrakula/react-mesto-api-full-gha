bce7f5fd4617b015abcdffa1b3ebb05631ce9905
Anton Pasyuk <atadrakula25@yandex.ru>
2023-08-09 20:12:37 +0300

    Develop/p15 test2

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,6 @@
 const serverConfig = {
-  url: 'https://api.web.portfolio.nomoreparties.co',
-  // url: 'http://localhost:3000',
+  // url: 'https://api.web.portfolio.nomoreparties.co',
+  url: 'http://localhost:3000',
   headers: {
     'Content-Type': 'application/json'
   }
